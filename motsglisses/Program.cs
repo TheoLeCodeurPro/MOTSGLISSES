f4e1bacf80c6ad801c644c9062314bc9b9f15910
TheoLeCodeurPro <theo.renouard8@gmail.com>
2023-12-05 16:09:00 +0100

    test

---
+++ motsglisses/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace motsglisses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
