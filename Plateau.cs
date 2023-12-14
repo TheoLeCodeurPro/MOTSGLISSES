9c523d932ff87dd3ba8e864cb92170297de59496
Demosthene-OR <olivier.renouard1103@gmail.com>
2023-12-14 18:08:57 +0100

    ....

--- Plateau.cs
+++ Plateau.cs
@@ -32,37 +32,35 @@ namespace motsglisses
                     plateau[i, j] = (char)('A' + random.Next(26)); ;
                 }
             }
+            try
             {
-                try
+                string repertoireCourant = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+                repertoireCourant = repertoireCourant + "/../../../Lettre.txt";
+                using (StreamReader reader = new StreamReader(repertoireCourant))
                 {
-                    string repertoireCourant = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
-                    repertoireCourant = repertoireCourant + "/../../../Lettre.txt";
-                    using (StreamReader reader = new StreamReader(repertoireCourant))
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
                     {
-                        string line;
-                        while ((line = reader.ReadLine()) != null)
+                        string[] parts = line.Split(',');
+
+                        if (parts.Length == 3)
                         {
-                            string[] parts = line.Split(',');
+                            char l = parts[0][0];
+                            int occMax = int.Parse(parts[1]);
+                            int poids = int.Parse(parts[2]);
 
-                            if (parts.Length == 3)
-                            {
-                                char l = parts[0][0];
-                                int occMax = int.Parse(parts[1]);
-                                int poids = int.Parse(parts[2]);
-
-                                this.lettre[l] = new LetterInfo(occMax, poids);
-                                Console.WriteLine(l+","+ lettre[l].OccMax+ "," + lettre[l].Poids);  
-                            }
+                            this.lettre[l] = new LetterInfo(occMax, poids);
+                            Console.WriteLine(l + "," + lettre[l].OccMax + "," + lettre[l].Poids);
                         }
                     }
                 }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Une erreur s'est produite lors de la lecture du fichier : {ex.Message}");
-                }
             }
-
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Une erreur s'est produite lors de la lecture du fichier : {ex.Message}");
+            }
         }
+
         public string toString()
         {
             string desplateau = "";
@@ -80,20 +78,19 @@ namespace motsglisses
         {
 
         }
-    }
 
-
-    // Classe pour stocker les informations associées à une lettre
-    public class LetterInfo
-    {
-        public int OccMax { get; set; }
-        public int Poids { get; set; }
-
-        // Constructeur
-        public LetterInfo(int occMax, int poids)
+        // Classe pour stocker les informations associées à une lettre
+        public class LetterInfo
         {
-            OccMax = occMax;
-            Poids = poids;
+            public int OccMax { get; set; }
+            public int Poids { get; set; }
+
+            // Constructeur
+            public LetterInfo(int occMax, int poids)
+            {
+                OccMax = occMax;
+                Poids = poids;
+            }
         }
     }
 }
